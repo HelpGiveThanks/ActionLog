db668c081abc5fcfe4e80c9a0dbbe4b93f970b31
David Matson <virtualdavid@users.noreply.github.com>
2013-12-14 10:19:34 -0800

    fix issue #54

--- Documentation/test.py
+++ Documentation/test.py
@@ -3,7 +3,7 @@ switchTestList
 #If the user selected the list currently showing then
 #stop the script.
 If [ TEMP::ktestItemList = TEMP::ktestItemListOLD ]
-github test 17
+github test 18
 #
 #switch to the selected test, which may or may
 #not have a test-item list.
