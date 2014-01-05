c41df5df7e8ddbe9876d2681d4d10734960a9d18
David Matson <githubdavid@gmail.com>
2014-01-05 01:28:05 -0800

    create new scripts

---
+++ Help/scripts/GoToSolutionsHelpScreen.py
@@ -0,0 +1,7 @@
+GoToSolutionsHelpScreen
+Set Variable [ $layout; Value:module::name ]
+Close Window [ Name: "Help Screens"; Current ?le ]
+Go to Layout [ $layout ]
+Omit Multiple Records [ Get (FoundCount) - 1 ]
+[ No dialog ]
+January 4, ??26 19:14:02 help.fp7 - GoToSolutionsHelpScreen -1-
