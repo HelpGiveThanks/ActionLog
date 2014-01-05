50f3725ea449be077ad081ca0bf1ea2689bed5ba
David Matson <githubdavid@gmail.com>
2014-01-05 01:41:39 -0800

    Update and rename rememberLayoutUserIsOn.py to rememberLayoutUserIsOn.c

--- Help/scripts/rememberLayoutUserIsOn.py
+++
@@ -1,7 +0,0 @@
-rememberLayoutUserIsOn
-Set Variable [ $$layout; Value:Get (LayoutName) ]
-If [ LeftValues ( tutorial::layoutName ; 1 ) ? Get (LayoutName) & ? ]
-Set Variable [ $name; Value:tutorial::layoutName ]
-Set Field [ tutorial::layoutName; Get (LayoutName) & ? & $name ]
-End If
-January 4, ??26 19:14:45 help.fp7 - rememberLayoutUserIsOn -1-
