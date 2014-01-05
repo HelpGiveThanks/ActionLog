054b56ea31dcfea25484c6cfc93e86d94030ec00
David Matson <githubdavid@gmail.com>
2014-01-05 01:36:12 -0800

    Update and rename GoToSolutionsHelpScreen.py to GoToSolutionsHelpScreen.c

--- Help/scripts/GoToSolutionsHelpScreen.c
+++ Help/scripts/GoToSolutionsHelpScreen.c
@@ -4,4 +4,3 @@ Close Window [ Name: "Help Screens"; Current ?le ]
 Go to Layout [ $layout ]
 Omit Multiple Records [ Get (FoundCount) - 1 ]
 [ No dialog ]
-January 4, ??26 19:14:02 help.fp7 - GoToSolutionsHelpScreen -1-
