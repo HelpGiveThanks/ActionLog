0a52ca03a206e34b6574145c70d10fb23f9e8acf
David Matson <githubdavid@gmail.com>
2014-01-06 00:58:08 -0800

    Create issueSortCategory.c

---
+++ ActionLog/scripts/sorts/issueSortCategory.c
@@ -0,0 +1,11 @@
+Sorts: issueSortCategory
+Sort Records [ Speciﬁed Sort Order: brainstate::description; ascending
+issueCategory::order; ascending
+issueCategory::text; ascending
+issueStatus::order; ascending
+issueStatus::text; ascending
+issue::order; based on value list: “1-99”
+issue::text; ascending ]
+[ Restore; No dialog ]
+Set Variable [ $$issueSort; Value:"category" ]
+January 6, 平成26 0:57:51 ActionLog.fp7 - issueSortCategory -1-
