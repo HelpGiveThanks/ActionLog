4310f1b820829bc6060e5f8792f54ce1d0f7afda
David Matson <githubdavid@gmail.com>
2014-01-05 19:47:14 -0800

    Create iphoneCut.c

---
+++ ActionLog/scripts/action-buttons/iphoneCut.c
@@ -0,0 +1,9 @@
+action buttons: iphoneCut
+// Cut [ ]
+[ Select ]
+Set Variable [ $$cut; Value:Get (ActiveFieldContents) ]
+Set Variable [ $$iphoneStop; Value:1 ]
+Set Field [ "" ]
+Go to Field [ ]
+Refresh Window
+January 5, 平成26 19:46:48 ActionLog.fp7 - iphoneCut -1-
