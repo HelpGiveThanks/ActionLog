1204b866b0b8e9e35fb5a622ae70c58d2c12ac60
David Matson <githubdavid@gmail.com>
2014-01-06 11:15:15 -0800

    Create newActPart2.c

---
+++ ActionLog/scripts/specific-action-log/newActPart2.c
@@ -0,0 +1,57 @@
+speciﬁc action log: newActPart2
+Allow User Abort [ Off ]
+Set Error Capture [ On ]
+#
+#Make key to ﬁt this group's lock.
+Set Variable [ $$group; Value:category::_LockList ]
+Set Variable [ $$pick ]
+Go to Field [ ]
+// Move/Resize Window [ Current Window; Height: Get (ScreenHeight); Top: 0; Left: Get (ScreenWidth) - 344 ]
+// Go to Layout [ “IssuesAndObservationsTag” (brainstate) ]
+Refresh Window
+#
+#Give key to current speciﬁc action.
+Select Window [ Name: "Speciﬁc Action"; Current ﬁle ]
+Set Field [ issue::_keyCategory; $$group ]
+Set Field [ issue::oldCategorykey; $$group ]
+#
+#These two keys are essential for the subtotal to work.
+Set Variable [ $$status; Value:issue::_keyStatus ]
+Set Field [ issue::sortTime; issueCategory::text ]
+#
+// #If user selected the same category that was already
+// #selected then stop this script.
+// If [ issue::_keyCategory = issue::oldCategorykey and $$stopSubtotal = "" ]
+// Go to Field [ ]
+// Exit Script [ ]
+// End If
+#
+Sort Records [ ]
+[ No dialog ]
+Refresh Window
+Perform Script [ “LoadIssuerecordID” ]
+Set Variable [ $$stopRecordLoad; Value:1 ]
+Set Variable [ $recordNumber; Value:Get ( RecordNumber ) ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Go to Record/Request/Page
+[ Next ]
+Set Variable [ $$stopRecordLoad ]
+Go to Record/Request/Page [ $recordNumber ]
+[ No dialog ]
+Go to Field [ issue::text ]
+January 6, 平成26 11:14:57 ActionLog.fp7 - newActPart2 -1-
