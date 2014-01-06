fca57744ee4bd1473dff045a9e8004647595c6fd
David Matson <githubdavid@gmail.com>
2014-01-05 19:38:32 -0800

    Create timerCloseWindow.c

---
+++ ActionLog/scripts/action-buttons/timerCloseWindow.c
@@ -0,0 +1,8 @@
+action buttons: timerCloseWindow
+#PURPOSE return user to previous layout and end the script which is done in the button setup by selecting the 'halt script' option.
+#
+#
+#
+Go to Layout [ $$alarmLayout ]
+Set Variable [ $$alarmLayout ]
+January 5, 平成26 19:38:11 ActionLog.fp7 - timerCloseWindow -1-
