2e8958d067a9958c1dd167783f6630cc84a9ac48
David Matson <githubdavid@gmail.com>
2014-01-06 01:07:39 -0800

    Create CloseWindow???.c

---
+++ ActionLog/scripts/not-sure-if-these-are-being-used/CloseWindow???.c
@@ -0,0 +1,5 @@
+not sure if these are being used: CloseWindow???
+#not sure if this is being used somewhere as a button
+Close Window [ Current Window ]
+Halt Script
+January 6, 平成26 1:07:21 ActionLog.fp7 - CloseWindow??? -1-
