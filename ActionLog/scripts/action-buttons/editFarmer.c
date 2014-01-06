5580abca4a6bfaa09101de4059999826224cb780
David Matson <githubdavid@gmail.com>
2014-01-05 19:54:51 -0800

    Create editFarmer.c

---
+++ ActionLog/scripts/action-buttons/editFarmer.c
@@ -0,0 +1,19 @@
+action buttons: editFarmer
+#PURPOSE go to createEdit farmer layout and show all users.
+#
+#
+#
+#1 go to the edit/create layout and show all users.
+Go to Layout [ “users” (steward) ]
+Show All Records
+Close Window [ Name: "Day"; Current ﬁle ]
+Close Window [ Name: "Speciﬁc Action"; Current ﬁle ]
+Close Window [ Name: "Tag"; Current ﬁle ]
+#
+#Set window title.
+Set Window Title [ Current Window; New Title: "HelpGiveThanks Solutions" ]
+#
+#Size window to 100%
+Set Zoom Level
+[ 100% ]
+January 5, 平成26 19:54:33 ActionLog.fp7 - editFarmer -1-
