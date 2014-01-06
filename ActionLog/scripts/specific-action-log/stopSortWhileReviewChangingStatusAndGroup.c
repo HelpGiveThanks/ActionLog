09d9fd51b0888b33bb0211532fc75af836b58bf4
David Matson <githubdavid@gmail.com>
2014-01-06 11:11:58 -0800

    Create stopSortWhileReviewChangingStatusAndGroup.c

---
+++ ActionLog/scripts/specific-action-log/stopSortWhileReviewChangingStatusAndGroup.c
@@ -0,0 +1,8 @@
+speciﬁc action log: stopSortWhileReviewChangingStatusAndGroup
+If [ $$stopSort = "" ]
+Set Variable [ $$stopSort; Value:1 ]
+Else
+Set Variable [ $$stopSort ]
+End If
+Refresh Window
+January 6, 平成26 11:11:43 ActionLog.fp7 - stopSortWhileReviewChangingStatusAndGroup -1-
