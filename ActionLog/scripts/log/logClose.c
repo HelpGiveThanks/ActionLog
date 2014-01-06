e830f009355855031c804bc49ba0a779cbfe0c8b
David Matson <githubdavid@gmail.com>
2014-01-05 19:31:45 -0800

    Create logClose.c

---
+++ ActionLog/scripts/log/logClose.c
@@ -0,0 +1,8 @@
+log: logClose
+Go to Layout [ $$loglayout ]
+Set Variable [ $$loglayout ]
+Go to Record/Request/Page
+[ First ]
+Scroll Window
+[ Home ]
+January 5, 平成26 19:31:29 ActionLog.fp7 - logClose -1-
