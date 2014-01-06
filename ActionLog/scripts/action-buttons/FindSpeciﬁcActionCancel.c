bc3e1976de4f47ca2f4c78590a529bceb29737f8
David Matson <githubdavid@gmail.com>
2014-01-05 19:50:30 -0800

    Create FindSpeciﬁcActionCancel.c

---
+++ ActionLog/scripts/action-buttons/FindSpeciﬁcActionCancel.c
@@ -0,0 +1,17 @@
+action buttons: FindSpeciﬁcActionCancel
+#If the user decides to cancel their the ﬁnd, then return
+#to the main record window and show all records.
+Go to Layout [ “Issues” (issue) ]
+Enter Find Mode [ ]
+Set Field [ issue::_keyBrainstate; $$logBrainstate ]
+Perform Find [ ]
+Show/Hide Status Area
+[ Hide ]
+Show/Hide Text Ruler
+[ Hide ]
+Sort Records [ ]
+[ No dialog ]
+Go to Record/Request/Page
+[ First ]
+Halt Script
+January 5, 平成26 19:50:04 ActionLog.fp7 - FindSpeciﬁcActionCancel -1-
