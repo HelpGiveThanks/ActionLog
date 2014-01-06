be46cda10e0037dbbe5f14737ab4e526e106e046
David Matson <githubdavid@gmail.com>
2014-01-06 01:07:03 -0800

    Create ShowInactiveToo????.c

---
+++ ActionLog/scripts/not-sure-if-these-are-being-used/ShowInactiveToo????.c
@@ -0,0 +1,14 @@
+not sure if these are being used: ShowInactiveToo????
+#not sure if this is being used somewhere
+Copy [ brainstate::_keyUser ]
+[ Select ]
+Enter Find Mode [ ]
+Paste [ brainstate::_keyUser ]
+[ Select ]
+Perform Find [ ]
+Sort Records [ Speciﬁed Sort Order: brainstate::description; ascending ]
+[ Restore; No dialog ]
+Go to Record/Request/Page
+[ First ]
+Go to Field [ brainstate::description ]
+January 6, 平成26 1:06:42 ActionLog.fp7 - ShowInactiveToo???? -1-
