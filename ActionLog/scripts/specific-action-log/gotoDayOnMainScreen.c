a04547b98c2234f41011208552c434a2f801ad8d
David Matson <githubdavid@gmail.com>
2014-01-06 01:16:25 -0800

    Create gotoDayOnMainScreen.c

---
+++ ActionLog/scripts/specific-action-log/gotoDayOnMainScreen.c
@@ -0,0 +1,29 @@
+speciﬁc action log: gotoDayOnMainScreen
+Set Variable [ $date; Value:logs::_keyDay ]
+Select Window [ Name: "Timer"; Current ﬁle ]
+#PURPOSE go back or forwards the number of days speciﬁed in the number ﬁeld to the right the go button on the 1 day layouts
+#
+#
+#
+#1 capture desired number of days and add to the current date. (User picks days from a drop down list that includes negative and positive
+numbers.)
+Set Field [ reference::day1; $date ]
+#
+#2 refresh the layout so that the key ﬁelds for each record will be recalculated based on the new day selected. The date and times shown for
+each record are in a table related to each brainstate record by their dates. The key date ﬁelds for all brainstate records are calculations
+where the value equals the reference ﬁeld date shown at the top of the screen in the green, red, or yellow box. So when the user changes
+the date in the reference ﬁeld (top colored box) and then this script refreshes the database, all the brainstate records including the ones not
+shown recalculate their key ﬁelds, the result being they all take in the new date. This means that the related day records now change too to
+show the user the day records that match the updated brainstate record keys. (These day ﬁelds are locked to their related brainstate
+records by the brainstate records ID number. Using these to keys, the database is able to return the desired day ﬁelds for brainstate record
+the user elects to view at any one time.)
+Refresh Window
+[ Flush cached join results ]
+#
+#3 capture user's current date perference
+Set Field [ steward::chosenDay; reference::day1 ]
+#
+#4 sort the records
+Sort Records [ ]
+[ No dialog ]
+January 6, 平成26 1:16:09 ActionLog.fp7 - gotoDayOnMainScreen -1-
