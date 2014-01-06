d147b0d3be26e3049866f86904b6bb4419ff43dd
David Matson <githubdavid@gmail.com>
2014-01-05 19:13:43 -0800

    Create goAndComeBackPart2.c

---
+++ ActionLog/scripts/navigation/goAndComeBackPart2.c
@@ -0,0 +1,8 @@
+navigation: goAndComeBackPart2
+#PURPOSE bounce back to previous layout via a script trigger on layout load.
+#
+#
+#
+#1 bounce back layout user just left.
+Go to Layout [ steward::chosenLayout ]
+January 5, 平成26 19:13:24 ActionLog.fp7 - goAndComeBackPart2 -1-
