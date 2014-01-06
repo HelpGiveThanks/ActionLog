073981821cc033da9577410c2e16894edf4fe6e1
David Matson <githubdavid@gmail.com>
2014-01-06 13:05:42 -0800

    Create updateNumberOfDaysSince.c

---
+++ ActionLog/scripts/action-buttons/updateNumberOfDaysSince.c
@@ -0,0 +1,10 @@
+action buttons: updateNumberOfDaysSince
+Show All Records
+Go to Record/Request/Page
+[ First ]
+Loop
+Perform Script [ “CHUNK_lastDayUsed” ]
+Go to Record/Request/Page
+[ Next; Exit after last ]
+End Loop
+January 6, 平成26 13:05:26 ActionLog.fp7 - updateNumberOfDaysSince -1-
