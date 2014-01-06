49e0466e434e3b65f77864a4efcf5b52a66388ff
David Matson <githubdavid@gmail.com>
2014-01-06 00:46:50 -0800

    Create sortAlpha.c

---
+++ ActionLog/scripts/sorts/sortAlpha.c
@@ -0,0 +1,43 @@
+Sorts: sortAlpha
+#PURPOSE of the 4 number and alpha ﬁelds, sort by the second one
+#
+#
+#
+If [ steward::chosenSort = "abc" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::sortAlpha; descending
+brainstate::sortSubNumber; descending
+brainstate::sortCategory; descending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "cba" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSort ≠ "abc" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::sortAlpha; based on value list: “sortAlpha”
+brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "abc" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:45:37 ActionLog.fp7 - sortAlpha -1-
