447dd7758cd9c18ad182c683c2c9617fb24094b5
David Matson <githubdavid@gmail.com>
2014-01-05 19:45:57 -0800

    Create newFarmer.c

---
+++ ActionLog/scripts/action-buttons/newFarmer.c
@@ -0,0 +1,11 @@
+action buttons: newFarmer
+#PURPOSE create a new record for the user
+#
+#
+#
+#1 create a new record
+New Record/Request
+#
+#2 put cursor in title text box for the user to enter ﬁrst name
+Go to Field [ steward::FirstName ]
+January 5, 平成26 19:45:19 ActionLog.fp7 - newFarmer -1-
