37aec629319e3ba4c54861c4ec872661fb1dfd6d
David Matson <githubdavid@gmail.com>
2014-01-06 11:10:58 -0800

    Create zoomMainOUT.c

---
+++ ActionLog/scripts/specific-action-log/zoomMainOUT.c
@@ -0,0 +1,12 @@
+speciﬁc action log: zoomMainOUT
+If [ Get ( WindowZoomLevel ) = 400 ]
+Set Zoom Level
+[ 300% ]
+Else If [ Get ( WindowZoomLevel ) = 300 ]
+Set Zoom Level
+[ 200% ]
+Else If [ Get ( WindowZoomLevel ) = 200 ]
+Set Zoom Level
+[ 150% ]
+End If
+January 6, 平成26 11:10:42 ActionLog.fp7 - zoomMainOUT -1-
