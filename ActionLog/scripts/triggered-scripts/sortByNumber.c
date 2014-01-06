cad72c6406855193587c3ae95a0d95cb3050368c
David Matson <githubdavid@gmail.com>
2014-01-06 01:03:18 -0800

    Create sortByNumber.c

---
+++ ActionLog/scripts/triggered-scripts/sortByNumber.c
@@ -0,0 +1,8 @@
+triggered scripts: sortByNumber
+Sort Records [ Speciﬁed Sort Order: brainstate::sortNumber; based on value list: “__-99”
+brainstate::sortAlpha; based on value list: “sortAlpha”
+brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+January 6, 平成26 1:03:02 ActionLog.fp7 - sortByNumber -1-
