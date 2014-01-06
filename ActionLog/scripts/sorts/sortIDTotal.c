b34c75d2db10de53fafc1ea988e5f41f96cb2971
David Matson <githubdavid@gmail.com>
2014-01-06 00:57:27 -0800

    Create sortIDTotal.c

---
+++ ActionLog/scripts/sorts/sortIDTotal.c
@@ -0,0 +1,37 @@
+Sorts: sortIDTotal
+#PURPOSE sort by ID
+#
+#
+#
+If [ steward::chosenSortTotal = "id" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::_lockBrainstateID; descending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "di" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSortTotal ≠ "id" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::_lockBrainstateID; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "id" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:57:09 ActionLog.fp7 - sortIDTotal -1-
