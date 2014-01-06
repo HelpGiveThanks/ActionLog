ca5d2b238b7e041637e96c79c5f1c7fdbed4050c
David Matson <githubdavid@gmail.com>
2014-01-06 11:16:44 -0800

    Create newItem.c

---
+++ ActionLog/scripts/specific-action-log/newItem.c
@@ -0,0 +1,7 @@
+speciﬁc action log: newItem
+Go to Layout [ “IssuesAndObservations Copy” (issue) ]
+New Record/Request
+Set Field [ issue::lock; "status" ]
+Go to Layout [ original layout ]
+Go to Field [ status::text ]
+January 6, 平成26 11:16:29 ActionLog.fp7 - newItem -1-
