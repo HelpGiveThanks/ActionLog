b1ff4a03252cbc511fde09ef236f973ba2e24fb0
David Matson <githubdavid@gmail.com>
2014-01-06 11:23:52 -0800

    Create gotoFieldItemPart3a.c

---
+++ ActionLog/scripts/specific-action-log/gotoFieldItemPart3a.c
@@ -0,0 +1,14 @@
+speciﬁc action log: gotoFieldItemPart3a
+#
+#If user clicked on the pulldown when the focus
+#of the system was on another record, stop this
+#script so user wont' get a useless ﬂash of the menu.
+If [ $$pulldownCheck ≠ Get (RecordID) ]
+Set Variable [ $$pulldownCheck; Value:Get (RecordID) ]
+Go to Field [ ]
+Exit Script [ ]
+End If
+Go to Object [ Object Name: 2 ]
+// Go to Field [ issue::_keyCategory ]
+[ Select/perform ]
+January 6, 平成26 11:23:34 ActionLog.fp7 - gotoFieldItemPart3a -1-
