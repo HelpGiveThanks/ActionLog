019f213e0bf654c4041ae656e116e32e1b2f5301
David Matson <githubdavid@gmail.com>
2014-01-06 01:00:37 -0800

    Create backup.c

---
+++ ActionLog/scripts/backup/backup.c
@@ -0,0 +1,10 @@
+backup: backup
+#PURPOSE remove focus from ﬁeld (activited by a layout trigger)
+#
+#
+#
+#1 set the ﬂag for backups to "backup" then open the BFData ﬁle
+Set Field [ backup::backup; "backup" ]
+Open File [ “MyData” ]
+[ Open hidden ]
+January 6, 平成26 1:00:13 ActionLog.fp7 - backup -1-
