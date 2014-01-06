e43a59d57765892b8c4a97ab6d99a8761870de0b
David Matson <githubdavid@gmail.com>
2014-01-06 01:04:53 -0800

    Create exitField.c

---
+++ ActionLog/scripts/triggered-scripts/exitField.c
@@ -0,0 +1,8 @@
+triggered scripts: exitField
+#PURPOSE remove focus from ﬁeld (activited by a layout trigger)
+#
+#
+#
+#1 remove focus from a ﬁeld
+Go to Field [ ]
+January 6, 平成26 1:04:36 ActionLog.fp7 - exitField -1-
