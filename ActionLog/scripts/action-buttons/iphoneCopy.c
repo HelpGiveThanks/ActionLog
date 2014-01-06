d3dbe28897d08264ed46c1555678138be5aeec98
David Matson <githubdavid@gmail.com>
2014-01-05 19:51:24 -0800

    Create iphoneCopy.c

---
+++ ActionLog/scripts/action-buttons/iphoneCopy.c
@@ -0,0 +1,6 @@
+action buttons: iphoneCopy
+Set Variable [ $$cut; Value:Get (ActiveFieldContents) ]
+Set Variable [ $$iphoneStop; Value:1 ]
+Go to Field [ ]
+Refresh Window
+January 5, 平成26 19:50:47 ActionLog.fp7 - iphoneCopy -1-
