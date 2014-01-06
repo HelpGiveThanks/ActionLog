c508703785ba4ff5b47ab065ff8876f5e4e70b52
David Matson <githubdavid@gmail.com>
2014-01-06 12:54:36 -0800

    Create CheckRepeatDates.c

---
+++ ActionLog/scripts/action sort checkboxes/CheckRepeatDates.c
@@ -0,0 +1,8 @@
+action sort checkboxes: CheckRepeatDates
+If [ brainstate::scheduleRepeatEventYear ≠ "" ]
+Show Custom Dialog [ Title: "pick one"; Message: "Choose to repeat item on speciﬁc weeks of the month OR every so many weeks, but
+not both."; Buttons: “OK” ]
+Set Field [ brainstate::scheduleRepeatEventMonth; "" ]
+Go to Field [ ]
+End If
+January 6, 平成26 12:53:53 ActionLog.fp7 - CheckRepeatDates -1-
