517684696beb81621b9ec4e2e08d7c8bfcec53e0
David Matson <githubdavid@gmail.com>
2014-01-06 01:05:25 -0800

    Create goToField.c

---
+++ ActionLog/scripts/triggered-scripts/goToField.c
@@ -0,0 +1,8 @@
+triggered scripts: goToField
+#PURPOSE remove focus from ﬁeld (activited by a layout trigger)
+#
+#
+#
+#1 remove focus from a ﬁeld
+Go to Field [ ]
+January 6, 平成26 1:05:08 ActionLog.fp7 - goToField -1-
