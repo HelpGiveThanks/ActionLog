7d00737b88ca1104efee97cf1a2da45d3b26f8cf
David Matson <githubdavid@gmail.com>
2014-01-06 11:19:00 -0800

    Create statusChangeDate.c

---
+++ ActionLog/scripts/specific-action-log/statusChangeDate.c
@@ -0,0 +1,5 @@
+speciﬁc action log: statusChangeDate
+Go to Field [ issue::dateDone ]
+Set Field [ issue::dateDone; Get ( CurrentTimeStamp ) ]
+Go to Field [ ]
+January 6, 平成26 11:18:41 ActionLog.fp7 - statusChangeDate -1-
