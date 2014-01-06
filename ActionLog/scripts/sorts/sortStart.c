bad9ce25bf1d7bdc0bedf4ee840527218c3f3a80
David Matson <githubdavid@gmail.com>
2014-01-06 00:48:51 -0800

    Create sortStart.c

---
+++ ActionLog/scripts/sorts/sortStart.c
@@ -0,0 +1,63 @@
+Sorts: sortStart
+#PURPOSE sort by start time
+#
+#
+#
+If [ steward::chosenSort = "allow" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+// Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+day1::swStart; descending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+brainstate::groupType; ascending
+day1::swStart; ascending
+brainstate::sortNumber; based on value list: “__-99”
+brainstate::sortAlpha; based on value list: “sortAlpha”
+brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "wolla" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSort ≠ "allow" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+// Sort Records [ Speciﬁed Sort Order: day1::swStart; ascending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+// Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+brainstate::groupType; ascending
+day1::swStart; ascending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+brainstate::groupType; ascending
+day1::swStart; descending
+brainstate::sortNumber; based on value list: “__-99”
+brainstate::sortAlpha; based on value list: “sortAlpha”
+brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "allow" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:48:31 ActionLog.fp7 - sortStart -1-
