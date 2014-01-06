2cbaecdf7ba70143ed197dee6d3eb170d22bed24
David Matson <githubdavid@gmail.com>
2014-01-05 19:33:55 -0800

    Create RemoveTextFormatting.c

---
+++ ActionLog/scripts/log/RemoveTextFormatting.c
@@ -0,0 +1,4 @@
+log: RemoveTextFormatting
+Set Field [ TextFontRemove ( (Get ( ActiveFieldContents )) ) ]
+Set Field [ TextFormatRemove ( (Get ( ActiveFieldContents )) ) ]
+January 5, 平成26 19:33:37 ActionLog.fp7 - RemoveTextFormatting -1-
