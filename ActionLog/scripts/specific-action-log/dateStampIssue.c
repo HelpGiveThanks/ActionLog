ac4ce65c40b435e0a533aa787d6fca4f0d7b1463
David Matson <githubdavid@gmail.com>
2014-01-06 01:18:31 -0800

    Create dateStampIssue.c

---
+++ ActionLog/scripts/specific-action-log/dateStampIssue.c
@@ -0,0 +1,4 @@
+speciﬁc action log: dateStampIssue
+Insert Calculated Result [ "[" & Get ( CurrentDate ) & "] "
+//TextSize ( "["; 12 ) & TextSize ( Get ( CurrentDate ) ; 12 ) & TextSize ( "] "; 12 ) ]
+January 6, 平成26 1:18:16 ActionLog.fp7 - dateStampIssue -1-
