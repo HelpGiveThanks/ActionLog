1c40505b7f58d65661945ec8c46d22e4ca74f1b2
David Matson <githubdavid@gmail.com>
2014-01-05 19:13:07 -0800

    Create goAndComeBackPart1.c

---
+++ ActionLog/scripts/navigation/goAndComeBackPart1.c
@@ -0,0 +1,13 @@
+navigation: goAndComeBackPart1
+#PURPOSE prevent huge time delay. (For some reason, when you select a new value in the go or scale dropdown lists, ﬁlemaker goes into a
+lengthy re-calc of all the barcharts on the layout, even though these drop downs are not connected in anyway with any of the record being
+calculated. To prevent this form happening, this script makes ﬁlemaker freeze the window and go to another layout. This other layout has
+a script layout trigger that causes the user to be returned to the layout that they just left. This back and forth between layouts seems to
+stop the recalc process from occuring.)
+#
+#
+#
+#1 freeze window and go to bounce back layout.
+Freeze Window
+Go to Layout [ “bounceBackUtilityLayout” (brainstate) ]
+January 5, 平成26 19:12:47 ActionLog.fp7 - goAndComeBackPart1 -1-
