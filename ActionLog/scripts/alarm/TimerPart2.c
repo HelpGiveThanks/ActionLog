d081485f5b51fd2b2a587fd67d7afb8790490545
David Matson <githubdavid@gmail.com>
2014-01-06 13:01:30 -0800

    Create TimerPart2.c

---
+++ ActionLog/scripts/alarm/TimerPart2.c
@@ -0,0 +1,20 @@
+alarm: TimerPart2
+If [ reference::repeatAlarm ≠ "" ]
+Perform Script [ “TimerRepeatAlarm” ]
+Halt Script
+End If
+Insert Current Time [ reference::TimerSet ]
+[ Select ]
+Go to Layout [ “timerAlarm” (steward) ]
+Go to Field [ ]
+Pause/Resume Script [ Duration (seconds): 11 ]
+Go to Layout [ “timerSetAlarm” (steward) ]
+Go to Field [ ]
+Pause/Resume Script [ Duration (seconds): reference::gDuration ]
+Go to Layout [ “timerAlarm” (steward) ]
+Adjust Window
+[ Hide ]
+Pause/Resume Script [ Duration (seconds): 11 ]
+Go to Layout [ $$alarmLayout ]
+// Close Window [ Name: "Predict Activity Time"; Current ﬁle ]
+January 6, 平成26 13:01:14 ActionLog.fp7 - TimerPart2 -1-
