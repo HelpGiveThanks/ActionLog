fb24ad6d5036978e267562fe903d342ce6b39e81
David Matson <githubdavid@gmail.com>
2014-01-06 12:55:57 -0800

    Create MarkAllDays.c

---
+++ ActionLog/scripts/action sort checkboxes/MarkAllDays.c
@@ -0,0 +1,5 @@
+action sort checkboxes: MarkAllDays
+Set Field [ brainstate::schedule; "" ]
+Set Field [ brainstate::schedule; "Sunday¶Monday¶Tuesday¶Wednesday¶Thursday¶Friday¶Saturday" ]
+Go to Field [ ]
+January 6, 平成26 12:55:37 ActionLog.fp7 - MarkAllDays -1-
