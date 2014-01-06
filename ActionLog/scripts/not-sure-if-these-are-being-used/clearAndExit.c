bbcaedbac85368e728ab62cd40b7962e4fc51d03
David Matson <githubdavid@gmail.com>
2014-01-06 01:06:22 -0800

    Create clearAndExit.c

---
+++ ActionLog/scripts/not-sure-if-these-are-being-used/clearAndExit.c
@@ -0,0 +1,5 @@
+not sure if these are being used: clearAndExit
+#not sure if this is being used somewhere as layout trigger
+Set Field [ "" ]
+Go to Field [ ]
+January 6, 平成26 1:05:51 ActionLog.fp7 - clearAndExit -1-
