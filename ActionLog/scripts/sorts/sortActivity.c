48eb8e6f6bbd8d53e73113ba06f27c63a525b13f
David Matson <githubdavid@gmail.com>
2014-01-06 00:50:02 -0800

    Create sortActivity.c

---
+++ ActionLog/scripts/sorts/sortActivity.c
@@ -0,0 +1,41 @@
+Sorts: sortActivity
+#PURPOSE sort by total time on the day layouts
+#
+#
+#
+If [ steward::chosenSort = "act" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+day1::swTotalActivity; descending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "tca" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSort ≠ "act" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+day1::swTotalActivity; ascending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSort; "act" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:49:44 ActionLog.fp7 - sortActivity -1-
