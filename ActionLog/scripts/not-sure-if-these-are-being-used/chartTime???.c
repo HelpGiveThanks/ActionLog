618e1e98da6da55e9ac83dd4584b347d49a85f12
David Matson <githubdavid@gmail.com>
2014-01-06 01:08:12 -0800

    Create chartTime???.c

---
+++ ActionLog/scripts/not-sure-if-these-are-being-used/chartTime???.c
@@ -0,0 +1,5 @@
+not sure if these are being used: chartTime???
+#not sure if this is being used somewhere
+Set Field [ steward::chartTime1; reference::chartTime ]
+Go to Field [ ]
+January 6, 平成26 1:07:53 ActionLog.fp7 - chartTime??? -1-
