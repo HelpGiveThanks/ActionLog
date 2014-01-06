88da090a860b34e843240a8aa0e4993ed3f1386a
David Matson <githubdavid@gmail.com>
2014-01-05 19:28:12 -0800

    Create gotoSpeciﬁcIssuesLog.c

---
+++ ActionLog/scripts/log/gotoSpeciﬁcIssuesLog.c
@@ -0,0 +1,11 @@
+log: gotoSpeciﬁcIssuesLog
+#PURPOSE open log windows
+#
+#basic administration tasks
+Set Error Capture [ On ]
+Allow User Abort [ Off ]
+#
+Select Window [ Name: "Tag"; Current ﬁle ]
+Select Window [ Name: "Day"; Current ﬁle ]
+Select Window [ Name: "Speciﬁc Action"; Current ﬁle ]
+January 5, 平成26 19:27:55 ActionLog.fp7 - gotoSpeciﬁcIssuesLog -1-
