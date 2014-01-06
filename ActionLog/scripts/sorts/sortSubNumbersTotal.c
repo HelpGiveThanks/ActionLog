debbde11fca6059d93b298ffe77fcc89bf0a7fa9
David Matson <githubdavid@gmail.com>
2014-01-06 00:53:32 -0800

    Create sortSubNumbersTotal.c

---
+++ ActionLog/scripts/sorts/sortSubNumbersTotal.c
@@ -0,0 +1,41 @@
+Sorts: sortSubNumbersTotal
+#PURPOSE of the 4 number and alpha ﬁelds, sort by the second to last one
+#
+#
+#
+If [ steward::chosenSortTotal = "sub" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::sortSubNumber; descending
+brainstate::sortCategory; descending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "bus" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSortTotal ≠ "sub" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "sub" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:53:12 ActionLog.fp7 - sortSubNumbersTotal -1-
