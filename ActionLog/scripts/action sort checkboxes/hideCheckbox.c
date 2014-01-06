d01e28c0317c7628b4240ed899d5f9faac4cdb03
David Matson <githubdavid@gmail.com>
2014-01-06 12:57:03 -0800

    Create hideCheckbox.c

---
+++ ActionLog/scripts/action sort checkboxes/hideCheckbox.c
@@ -0,0 +1,8 @@
+action sort checkboxes: hideCheckbox
+If [ brainstate::groupType ≠ "" ]
+Set Field [ brainstate::hide; "t" ]
+Go to Field [ ]
+Halt Script
+End If
+Go to Field [ ]
+January 6, 平成26 12:56:33 ActionLog.fp7 - hideCheckbox -1-
