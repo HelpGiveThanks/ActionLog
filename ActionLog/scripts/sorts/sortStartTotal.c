baffbb1f911bed6d322f82058ef4a28b4e400df5
David Matson <githubdavid@gmail.com>
2014-01-06 00:55:08 -0800

    Create sortStartTotal.c

---
+++ ActionLog/scripts/sorts/sortStartTotal.c
@@ -0,0 +1,40 @@
+Sorts: sortStartTotal
+#PURPOSE sort by start time
+#
+#
+#
+If [ steward::chosenSortTotal = "allow" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+day1::swStart; descending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "wolla" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+Halt Script
+End If
+If [ steward::chosenSortTotal ≠ "allow" ]
+Set Variable [ $$record; Value:brainstate::_lockBrainstateID ]
+Sort Records [ Speciﬁed Sort Order: day1::swStart; ascending
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Set Field [ steward::chosenSortTotal; "allow" ]
+Go to Record/Request/Page
+[ First ]
+Loop
+Exit Loop If [ $$record = brainstate::_lockBrainstateID ]
+Go to Record/Request/Page
+[ Next ]
+End Loop
+Set Variable [ $$record ]
+Go to Field [ ]
+End If
+January 6, 平成26 0:54:47 ActionLog.fp7 - sortStartTotal -1-
