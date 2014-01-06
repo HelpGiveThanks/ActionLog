75fa95fed56ac07ebe7931d4dfa34249215196db
David Matson <githubdavid@gmail.com>
2014-01-05 19:57:46 -0800

    Create time.c

---
+++ ActionLog/scripts/action-buttons/time.c
@@ -0,0 +1,23 @@
+action buttons: time
+#PURPOSE script attached to the time button on the edit time layouts. It checks if user is in a time ﬁeld, and if they are inserts the current time
+into it, and if they are not tells them how to use the time button.
+#
+#
+If [ brainstate::_lockBrainstateID = brainstate::groupID ]
+Go to Field [ ]
+Halt Script
+End If
+#
+#1 determine if the user is in a time ﬁeld
+If [ Get ( ActiveFieldName ) = "swPause" or Get ( ActiveFieldName ) = "swStart" ]
+#
+#2 if they are then insert the current time into it
+Insert Current Time [ ]
+[ Select ]
+Go to Field [ ]
+Halt Script
+End If
+#
+#2 if they are not tell them how to use this button
+Show Custom Dialog [ Title: "!"; Message: "This button is for inserting the current time into a stop or start time ﬁeld."; Buttons: “OK” ]
+January 5, 平成26 19:57:24 ActionLog.fp7 - time -1-
