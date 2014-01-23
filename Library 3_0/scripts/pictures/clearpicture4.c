d694fcb262d74f33c4d66736de258593f876bc74
David Matson <githubdavid@gmail.com>
2014-01-23 04:44:16 -0800

    update library

--- Library 3_0/scripts/pictures/clearpicture4.c
+++
@@ -1,8 +0,0 @@
-pictures: clearpicture4
-#
-#
-#
-Go to Field [ report::_kfPhoto4 ]
-Perform Script [ “reportClearDiscoveryLink” ]
-Go to Field [ ]
-January 7, 平成26 17:09:02 Imagination Quality Management.fp7 - clearpicture4 -1-
