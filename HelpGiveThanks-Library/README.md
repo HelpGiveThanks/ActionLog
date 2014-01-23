d5350409d29fb8e63bb47bcd89b5d1d013ef5fab
David Matson <githubdavid@gmail.com>
2014-01-23 04:47:45 -0800

    update folder names

--- HelpGiveThanks-Library/README.md
+++
@@ -1,2 +0,0 @@
-HelpGiveThanks-Library
-======================
--- HelpGiveThanks-Library/scripts/tagMenu/findModeCancel.c
+++
@@ -1,8 +0,0 @@
-tagMenu: FindModeCancel
-#
-#Script activitated if user cancels ﬁnd and returns user
-#to layout they were viewing.
-Go to Layout [ $$ﬁndLayout ]
-Set Variable [ $$ﬁndLayout ]
-#
-January 7, 平成26 16:39:37 Imagination Quality Management.fp7 - FindModeCancel -1-
