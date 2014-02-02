619b958dcad4777b32fb21aedafc240e19d203be
David Matson <githubdavid@gmail.com>
2014-02-01 21:56:51 -0800

    Fixed problem with copying script from Chrome
    Had to remove dash in line number 24 which used to read re- open and
    now reads open to fix problem with Chrome preventing copy/paste.

--- ActionLog/scripts/navigation/gotoLibraryDatabase.c
+++ ActionLog/scripts/navigation/gotoLibraryDatabase.c
@@ -9,25 +9,26 @@ Select Window [ Name: "References" ]
 Select Window [ Name: MemorySwitch::currentLibraryMainWIndow ]
 #
 #If no library is open, then open the last library used.
-If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath ≠ "" ]
+If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath ≠ "" ]
 Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
 Substitute (MemorySwitch::currentLibraryPath ; " " ; "%20" ) ;
-Substitute ( Substitute ( MemorySwitch::currentLibraryPath ; "file:/" ; "file://" ) ; " " ; "%20" ) ) ] [ No dialog ]
+Substitute ( Substitute ( MemorySwitch::currentLibraryPath ; "ﬁle:/" ; "ﬁle://" ) ; " " ; "%20" ) ) ]
+[ No dialog ]
 #
-#If the path stored for the selected library failed to open 
-#the library, then that library is no longer there or its 
-#name has been changed, so inform the user of their 
+#If the path stored for the selected library failed to open
+#the library, then that library is no longer there or its
+#name has been changed, so inform the user of their
 #options.
 If [ Get (LastError) = 5 ]
-Show Custom Dialog [ Message: "Last library used cannot be found as it must have been renamed, moved, or deleted. Find and re- open it manually or click the allsolutions button to select another library."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "Last library used cannot be found as it must have been renamed, moved, or deleted. Find and
+open it manually or click the allsolutions button to select another library."; Buttons: “OK” ]
 End If
 #
-#If their are no libraries in the system let the user 
+#If their are no libraries in the system let the user
 #know how to add new ones.
 Else If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath = "" ]
-Show Custom Dialog [ Message: "There is no library file in memory. Click on a library file manually and it will be added to memory and
-￼End If
-#
+Show Custom Dialog [ Message: "There is no library ﬁle in memory. Click on a library ﬁle manually and it will be added to memory and
 opened the next time you click the library button."; Buttons: “OK” ]
-February 1, 平成26 18:15:55
-ActionLog.fp7 - gotoLibraryDatabase -1-
+End If
+#
+February 1, 平成26 21:48:57 ActionLog.fp7 - gotoLibraryDatabase -1-
