e51e4dfb6d326e4351b8e64f34b2e0abae4e329d
David Matson <githubdavid@gmail.com>
2014-02-03 12:09:35 -0800

    Fix non Chrome PDF changes to script
    I used preview instead of Chrome to open this scripts PDF because I did
    not know this issue
    https://code.google.com/p/chromium/issues/detail?id=327349&can=1&q=copy%
    20pdf%20clipboard&colspec=ID%20Pri%20M%20Iteration%20ReleaseBlock%20Cr%2
    0Status%20Owner%20Summary%20OS%20Modified
    
    This is part two of fixing this mistake, by now updating GitHub with
    the changes as copied out of Chrome and into TeTextMate.

--- ActionLog/scripts/navigation/gotoLibraryDatabase.c
+++ ActionLog/scripts/navigation/gotoLibraryDatabase.c
@@ -1,106 +1,34 @@
 navigation: gotoLibraryDatabase
-// Select Window [ Name: "Test" ]
-// Select Window [ Name: "Learn" ]
-// Select Window [ Name: "Setup" ]
-// Select Window [ Name: "Report" ]
-// Select Window [ Name: "References" ]
-Select Window [ Name: MemorySwitch::currentLibraryMainWIndow ]
-If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath ≠ "" ]
-Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
-Substitute (MemorySwitch::currentLibraryPath ; " " ; "%20" ) ;
-Substitute ( Substitute ( MemorySwitch::currentLibraryPath ; "ﬁle:/" ; "ﬁle://" ) ; " " ; "%20" ) ) ]
-[ No dialog ]
-If [ Get (LastError) = 5 ]
-Set Variable [ $$stopRecordLoad; Value:1 ]
-New Window [ Name: "library" ]
-Go to Layout [ “MemorySwitch” (MemorySwitch) ]
-Set Variable [ $$stopRecordLoad ]
-Go to Record/Request/Page
-[ First ]
-Loop
-Exit Loop If [ Get (FoundCount) = 0 ]
 #
-#These disabled script steps deleted all invalid paths
-#to libraries and opened the ﬁrst path that was valid.
-// Open URL [ Substitute ( Substitute ( MemorySwitch::path ; "ﬁle:/" ; "ﬁle://" ) ; " " ; "%20" ) ]
-[ No dialog ]
-// Exit Loop If [ Get (LastError) = 0 ]
-// Delete Record/Request
-[ No dialog ]
-// Exit Loop If [ Get (LastError) = 101 ]
-Exit Loop If [ MemorySwitch::currentLibraryPath = MemorySwitch::path ]
-Go to Record/Request/Page
-[ Next; Exit after last ]
-End Loop
-If [ MemorySwitch::currentLibraryPath = MemorySwitch::path ]
-Delete Record/Request
-[ No dialog ]
-End If
-Set Variable [ $found; Value:Get (FoundCount) ]
-Close Window [ Current Window ]
-If [ $found = 0 ]
-Show Custom Dialog [ Message: "There is no library ﬁle in memory. Open one up manually, and it will be added to memory and
-opened when you next click the library button."; Buttons: “OK” ]
-Else
-Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle:/" )
-& "library/library." & Right ( Get ( FilePath ) ; 3 )
- ; " " ; "%20" ) ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle://" )
-& "library/library." & Right ( Get ( FilePath ) ; 3 )
- ; " " ; "%20" ) ) ]
-[ No dialog ]
-If [ Get (LastError) = 5 ]
-Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle:/" )
-& "library/library.USR"
- ; " " ; "%20" ) ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle://" )
-& "library/library.USR"
- ; " " ; "%20" ) ) ]
-[ No dialog ]
-February 2, 平成26 13:22:16 ActionLog[Conﬂict].fp7 - gotoLibraryDatabase -1-navigation: gotoLibraryDatabase
-Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle:/" )
-& "library/library.USR"
- ; " " ; "%20" ) ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle://" )
-& "library/library.USR"
- ; " " ; "%20" ) ) ]
-[ No dialog ]
-If [ Get (LastError) = 5 ]
-Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle:/" )
-& "library/library.USR"
- ; " " ; "%20" ) ;
-Substitute (
-Substitute (Left (Get (FilePath) ; Length ( Get (FilePath) ) - ( Length ( Get (FileName) ) + 4 ) ) ; "ﬁle:/" ; "ﬁle:///" )
-& "library/library.USR"
- ; " " ; "%20" ) ) ]
-[ No dialog ]
-End If
-End If
-If [ Get (LastError) = 5 ]
-Show Custom Dialog [ Message: "The library you last looked at cannot be opened because its name was changed, the ﬁle
-was moved, or deleted. Open a library ﬁle up manually, and it will be added to memory and opened when you next
-click the library button."; Buttons: “OK” ]
-End If
-End If
-End If
-Else If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath = "" ]
-Show Custom Dialog [ Message: "There is no library ﬁle in memory. Open one up, and it will be added to memory to be opened in when
-you next click the library button."; Buttons: “OK” ]
-End If
+#Select the current library windows if open and selected.
 Select Window [ Name: "Tag Menus" ]
 Select Window [ Name: "Learn" ]
 Select Window [ Name: "Setup" ]
 Select Window [ Name: "Report" ]
 Select Window [ Name: "References" ]
-February 2, 平成26 13:22:16 ActionLog[Conﬂict].fp7 - gotoLibraryDatabase -2-
+Select Window [ Name: MemorySwitch::currentLibraryMainWIndow ]
+#
+#If no library is open, then open the last library used.
+If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath ≠ "" ]
+Open URL [ Case ( Get ( SystemPlatform ) = - 2 ;
+Substitute (MemorySwitch::currentLibraryPath ; " " ; "%20" ) ;
+Substitute ( Substitute ( MemorySwitch::currentLibraryPath ; "ﬁle:/" ; "ﬁle://" ) ; " " ; "%20" ) ) ]
+[ No dialog ]
+#
+#If the path stored for the selected library failed to open
+#the library, then that library is no longer there or its
+#name has been changed, so inform the user of their
+#options.
+If [ Get (LastError) = 5 ]
+Show Custom Dialog [ Message: "Last library used cannot be found as it must have been renamed, moved, or deleted. Find and
+open it manually or click the allsolutions button to select another library."; Buttons: “OK” ]
+End If
+#
+#If their are no libraries in the system let the user
+#know how to add new ones.
+Else If [ Get (LastError) = 112 and MemorySwitch::currentLibraryPath = "" ]
+Show Custom Dialog [ Message: "There is no library ﬁle in memory. Click on a library ﬁle manually and it will be added to memory and
+opened the next time you click the library button."; Buttons: “OK” ]
+End If
+#
+February 2, 平成26 14:09:29 ActionLog.fp7 - gotoLibraryDatabase -1-
