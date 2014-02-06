26f58fa84de66a2b4c67ed275768c11d73d649e4
David Matson <githubdavid@gmail.com>
2014-02-06 12:37:11 -0800

    Add sort for TimerEdit screen << and >> buttons
    Fix <<  >> buttons on Timer Edit layout so script sorts active timers
    for the day to the top as users change days.  Made long line comment in
    script multiple lines for easy viewing.

--- ActionLog/scripts/navigation/goPlus1.c
+++ ActionLog/scripts/navigation/goPlus1.c
@@ -6,14 +6,27 @@ navigation: goPlus1
 #1 add one the current date.
 Set Field [ reference::day1; reference::day1 + 1 ]
 #
-#2 refresh the layout so that the key ﬁelds for each record will be recalculated based on the new day selected. The date and times shown for
-each record are in a table related to each brainstate record by their dates. The key date ﬁelds for all brainstate records are calculations
-where the value equals the reference ﬁeld date shown at the top of the screen in the green, red, or yellow box. So when the user changes
-the date in the reference ﬁeld (top colored box) and then this script refreshes the database, all the brainstate records including the ones not
-shown recalculate their key ﬁelds, the result being they all take in the new date. This means that the related day records now change too to
-show the user the day records that match the updated brainstate record keys. (These day ﬁelds are locked to their related brainstate
-records by the brainstate records ID number. Using these to keys, the database is able to return the desired day ﬁelds for brainstate record
-the user elects to view at any one time.)
+#2 refresh the layout so that the key ﬁelds for each
+#record will be recalculated based on the new day
+#selected. The date and times shown for each
+#record are in a table related to each brainstate
+#record by their dates. The key date ﬁelds for all
+#brainstate records are calculations where the value
+# equals the reference ﬁeld date shown at the top of
+#the screen in the green, red, or yellow box. So
+#when the user changes the date in the reference
+#ﬁeld (top colored box) and then this script
+#refreshes the database, all the brainstate records
+#including the ones not shown recalculate their key
+# ﬁelds, the result being they all take in the new
+#date. This means that the related day records now
+# change too to show the user the day records that
+#match the updated brainstate record keys. (These
+# day ﬁelds are locked to their related brainstate
+#records by the brainstate records ID number.
+# Using these two keys, the database is able to return
+# the desired day ﬁelds for the brainstate record the
+#user elects to view at any time.)
 Refresh Window
 [ Flush cached join results ]
 #
@@ -26,10 +39,22 @@ only done if the user wants the records sorted by time, in which case after the 
 // Perform Script [ “sortRestoreAfterScript” ]
 #
 #4 sort the records
+If [ Get ( LayoutName ) = "01EditTime" ]
+Sort Records [ Speciﬁed Sort Order: day1::_keyDay; ascending
+brainstate::groupType; ascending
+day1::swStart; ascending
+brainstate::sortNumber; based on value list: “__-99”
+brainstate::sortAlpha; based on value list: “sortAlpha”
+brainstate::sortSubNumber; based on value list: “__-99”
+brainstate::sortCategory; based on value list: “sortAlpha”
+brainstate::description; ascending ]
+[ Restore; No dialog ]
+Else
 Sort Records [ Speciﬁed Sort Order: brainstate::sortNumber; based on value list: “__-99”
 brainstate::sortAlpha; based on value list: “sortAlpha”
 brainstate::sortSubNumber; based on value list: “__-99”
 brainstate::sortCategory; based on value list: “sortAlpha”
 brainstate::description; ascending ]
 [ Restore; No dialog ]
-January 5, 平成26 14:30:53 ActionLog.fp7 - goPlus1 -1-
+End If
+February 6, 平成26 12:33:09 ActionLog.fp7 - goPlus1 -1-
