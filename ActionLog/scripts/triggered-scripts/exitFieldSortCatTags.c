b783f6b994a67bb396389bfe84fff7d4f5a53dd4
David Matson <githubdavid@gmail.com>
2016-01-04 03:15:11 -0800

    Modified script to sort tag order number first.

--- ActionLog/scripts/triggered-scripts/exitFieldSortCatTags.c
+++ ActionLog/scripts/triggered-scripts/exitFieldSortCatTags.c
@@ -5,8 +5,9 @@ triggered scripts: exitFieldSortCatTags
 #
 #1 remove focus from a field
 Go to Field [ ]
-Sort Records [ Specified Sort Order: group::text; ascending
+Sort Records [ Specified Sort Order: group::order; based on value list: “1-99”
+group::text; ascending
 category::sortTime; ascending
 category::text; ascending ]
 [ Restore; No dialog ]
-December 6, ଘ౮27 21:46:06 ActionLog.fp7 - exitFieldSortCatTags -1-
+January 4, ଘ౮28 3:13:12 ActionLog.fp7 - exitFieldSortCatTags -1-
